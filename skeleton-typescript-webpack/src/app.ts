64a0eacb0da26587ad806d3c3e9b38d8305872e2
Jishnu Viswanath <neo.9801@gmail.com>
2016-11-23 11:51:51 +0530

    Bug Report

--- skeleton-typescript-webpack/src/app.ts
+++ skeleton-typescript-webpack/src/app.ts
@@ -5,9 +5,9 @@ export class App {
   router: Router;
 
   configureRouter(config: RouterConfiguration, router: Router) {
-    config.title = 'Aurelia';
+    config.title = 'Neo';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: './welcome',      nav: true, title: 'Welcome' },
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: './welcome',      nav: true, title: 'Ola' },
       { route: 'users',         name: 'users',        moduleId: './users',        nav: true, title: 'Github Users' },
       { route: 'child-router',  name: 'child-router', moduleId: './child-router', nav: true, title: 'Child Router' }
     ]);
--- skeleton-typescript-webpack/src/child-router.ts
+++ skeleton-typescript-webpack/src/child-router.ts
@@ -7,9 +7,15 @@ export class ChildRouter {
 
   configureRouter(config: RouterConfiguration, router: Router) {
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',       moduleId: './welcome',       nav: true, title: 'Welcome' },
+      {
+        route: '',
+        redirect: 'welcome'
+      },
+      { route: ['welcome'], name: 'welcome',       moduleId: './welcome',       nav: true, title: 'Ola2' },
       { route: 'users',         name: 'users',         moduleId: './users',         nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router',  moduleId: './child-router',  nav: true, title: 'Child Router' }
+      { route: 'child-router',  name: 'child-router',  moduleId: './child-router',  nav: true, title: 'Child 1' },
+      { route: 'child-router/:id',  name: 'child-router2',  moduleId: './child-router',  nav: false, title: 'Child' }
+
     ]);
 
     this.router = router;
